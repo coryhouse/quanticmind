bd97bc5be7317bb08643a6ce66a45ec17de58a91
Cory House <housecor@gmail.com>
2018-08-13 10:33:19 -0500

    Default props

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -2,19 +2,25 @@ import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const PageNotFound = ({ location }) => {
+const PageNotFound = ({ location, message }) => {
   return (
     <Fragment>
       <h1>Oops! {location.pathname} was not found.</h1>
       <p>
         <Link to="/">Go home</Link>
       </p>
+      <p>{message}</p>
     </Fragment>
   );
 };
 
 PageNotFound.propTypes = {
-  location: PropTypes.object.isRequired
+  location: PropTypes.object.isRequired,
+  message: PropTypes.string
+};
+
+PageNotFound.defaultProps = {
+  message: "Sorry, it's likely Amit's fault."
 };
 
 export default PageNotFound;
